36632d6220b4bcc1bbb5fc0934a9db3737a10c0a
Lucas Montes <lucas.montes.lm@gmail.com>
2022-02-23 18:03:00 -0300

    Refazendo o solicitado

--- 04-Paint/RectAndEllipse/RectEllipseApp.java
+++ 04-Paint/RectAndEllipse/RectEllipseApp.java
@@ -31,29 +31,29 @@ class RectEllipseFrame extends JFrame {
         );
         this.setTitle("Rect + Ellipse");
         this.setSize(350, 350);
-        this.r1 = new Rect(30,40, 100,30);
-        this.r2 = new Rect(60, 80, 70, 20);
-        this.r3 = new Rect(120, 150, 80, 70);
-        this.r4 = new Rect(50, 150, 20, 40);
-        this.e1 = new Ellipse(150,100, 100,30);
+        this.r1 = new Rect(30,40, 100,30, new Color(0, 255, 0), new Color(255, 0, 0));
+        this.r2 = new Rect(60, 80, 70, 20, new Color(117, 0, 0), new Color(117, 169, 0));
+        this.r3 = new Rect(120, 150, 80, 70, new Color(80, 93, 195), new Color(3, 129, 9));
+        this.r4 = new Rect(50, 150, 20, 40, new Color(152, 129, 9), new Color(152, 202, 236));
         //Criando novos exemplos
-        this.e2 = new Ellipse(220, 150, 40, 60);
-        this.e3 = new Ellipse(190, 250, 135, 80);
-        this.e4 = new Ellipse(50, 200, 70, 120);
+        this.e1 = new Ellipse(150,100, 100,30, new Color(110, 0, 0), new Color(65, 169, 0) );
+        this.e2 = new Ellipse(220, 150, 40, 60, new Color(90, 202, 236), new Color(195, 128, 236));
+        this.e3 = new Ellipse(190, 250, 135, 80, new Color(18, 244, 70), new Color(242, 244, 70));
+        this.e4 = new Ellipse(50, 200, 70, 120, new Color(119, 49, 70), new Color(241, 153, 70));
     }
 
     public void paint (Graphics g) {
         super.paint(g);
         //Documentacao: g, back, line
-        this.r1.paint(g, new Color(0, 255, 0), new Color(255, 0, 0));
-        this.r2.paint(g, new Color(117, 0, 0), new Color(117, 169, 0));
-        this.r3.paint(g, new Color(80, 93, 195), new Color(3, 129, 9));
-        this.r4.paint(g, new Color(152, 129, 9), new Color(152, 202, 236));
+        this.r1.paint(g);
+        this.r2.paint(g);
+        this.r3.paint(g);
+        this.r4.paint(g);
 
-        this.e1.paint(g, new Color(90, 202, 236), new Color(195, 128, 236));
-        this.e2.paint(g, new Color(18, 244, 70), new Color(242, 244, 70));
-        this.e3.paint(g, new Color(119, 49, 70), new Color(241, 153, 70));
-        this.e4.paint(g, new Color(111, 153, 70), new Color(227, 47, 70));
+        this.e1.paint(g);
+        this.e2.paint(g);
+        this.e3.paint(g);
+        this.e4.paint(g);
     }
 }
 
@@ -62,11 +62,13 @@ class Rect {
     int w, h;
     Color back, line;
 
-    Rect (int x, int y, int w, int h) {
+    Rect (int x, int y, int w, int h, Color back, Color line) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
+        this.back = back;
+        this.line = line;
     }
 
 	void print () {
@@ -74,10 +76,8 @@ class Rect {
             this.w, this.h, this.x, this.y);
     }
 
-    void paint (Graphics g, Color back, Color line) {
+    void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        this.back = back;
-        this.line = line;
         g2d.setStroke(new BasicStroke(7));
         g2d.setColor(this.line);
         g2d.drawRect(this.x,this.y, this.w,this.h);
@@ -91,11 +91,13 @@ class Ellipse {
     int w, h;
     Color back, line;
 
-    Ellipse (int x, int y, int w, int h) {
+    Ellipse (int x, int y, int w, int h, Color back, Color line) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
+        this.back = back;
+        this.line = line;
     }
 
     void print () {
@@ -103,11 +105,9 @@ class Ellipse {
             this.w, this.h, this.x, this.y);
     }
 
-    void paint (Graphics g, Color back, Color line) {
+    void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
         g2d.setStroke(new BasicStroke(7));
-        this.back = back;
-        this.line = line;
         g2d.setColor(this.line);
         g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
         g2d.setColor(this.back);
